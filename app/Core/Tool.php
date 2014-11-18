74991ebaa38cca8bc4a13d439ddd7b2a839d18ef
Jan Rosier <rosier@interstroom.nl>
2014-11-18 16:45:32 +0100

    Write a Windows CSV file

--- app/Core/Tool.php
+++ app/Core/Tool.php
@@ -11,21 +11,28 @@ namespace Core;
 class Tool
 {
     /**
-     * Write a CSV file
+     * Write a Windows CSV file
      *
      * @static
      * @access public
      * @param  array    $rows       Array of rows
      * @param  string   $filename   Output filename
      */
-    public static function csv(array $rows, $filename = 'php://output')
+    public static function csv(array $rows, $filename = '')
     {
-        $fp = fopen($filename, 'w');
+        $fp = empty($filename) ? tmpfile() : fopen($filename, 'w');
 
         if (is_resource($fp)) {
 
             foreach ($rows as $fields) {
                 fputcsv($fp, $fields);
+                fseek($fp, -1, SEEK_CUR);
+                fwrite($fp, "\r\n");
+            }
+
+            if (empty($filename)) {
+                rewind($fp);
+                fpassthru($fp);
             }
 
             fclose($fp);
